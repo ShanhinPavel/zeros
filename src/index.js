34d3c60efdc03f2261576a6c3545c4f81411bd02
ShanhinPavel <shanMail@tut.by>
2018-03-02 00:52:19 +0300

    Seventh task has solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var x = number;
+  var n = number;
+  var counter_5s = 0;
+  
+
+while(x>0) {
+  var counter_5 = 0;
+n=x;
+while(n > 0 && n % 5 == 0) {
+	counter_5++;
+  n = n / 5;
 }
+x--;
+counter_5s =counter_5s +counter_5;
+}
+
+return counter_5s;
+ }
+
